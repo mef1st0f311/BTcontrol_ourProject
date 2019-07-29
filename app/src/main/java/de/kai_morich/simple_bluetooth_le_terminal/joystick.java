9fedbbbbe2ba4a857b186ffcd2ddba32b5f0d3d2
mef1st0f311 <hardity2903.gmail.com>
2019-07-29 20:56:14 +0300

    test11
    test

--- app/src/main/java/de/kai_morich/simple_bluetooth_le_terminal/joystick.java
+++ app/src/main/java/de/kai_morich/simple_bluetooth_le_terminal/joystick.java
@@ -13,173 +13,166 @@ import com.zerokol.views.joystickView.JoystickView.OnJoystickMoveListener;
 import java.util.Timer;
 import java.util.TimerTask;
 
-public class joystick extends Activity{
-
-
+public class joystick extends Activity {
 
 
     //public class MainActivity extends Activity {
 
-        TextView tvText;
-        SensorManager sensorManager;
-        Sensor sensorAccel;
-        Sensor sensorLinAccel;
-        Sensor sensorGravity;
-        private TextView angleTextView;
-        private TextView powerTextView;
-        private TextView directionTextView;
-        // Importing also other views
-        private JoystickView joystick;
+    TextView tvText;
+    SensorManager sensorManager;
+    Sensor sensorAccel;
+    Sensor sensorLinAccel;
+    Sensor sensorGravity;
+    private TextView angleTextView;
+    private TextView powerTextView;
+    private TextView directionTextView;
+    // Importing also other views
+    private JoystickView joystick;
 
-        StringBuilder sb = new StringBuilder();
+    StringBuilder sb = new StringBuilder();
 
-        Timer timer;
+    Timer timer;
 
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_main);
-            tvText = (TextView) findViewById(R.id.tvText);
-            sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
-            sensorAccel = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-            sensorLinAccel = sensorManager
-                    .getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION);
-            sensorGravity = sensorManager.getDefaultSensor(Sensor.TYPE_GRAVITY);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        tvText = (TextView) findViewById(R.id.tvText);
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        sensorAccel = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        sensorLinAccel = sensorManager
+                .getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION);
+        sensorGravity = sensorManager.getDefaultSensor(Sensor.TYPE_GRAVITY);
 //            angleTextView = (TextView) findViewById(R.id.angleTextView);
 //            powerTextView = (TextView) findViewById(R.id.powerTextView);
 //            directionTextView = (TextView) findViewById(R.id.directionTextView);
-            //Referencing also other views
-            joystick = (JoystickView) findViewById(R.id.joystickView);
-            //Event listener that always returns the variation of the angle in degrees, motion power in percentage and direction of movement
-            joystick.setOnJoystickMoveListener(new OnJoystickMoveListener() {
+        //Referencing also other views
+        joystick = (JoystickView) findViewById(R.id.joystickView);
+        //Event listener that always returns the variation of the angle in degrees, motion power in percentage and direction of movement
+        joystick.setOnJoystickMoveListener(new OnJoystickMoveListener() {
 
-                @Override
-                public void onValueChanged(int angle, int power, int direction) {
-                    // TODO Auto-generated method stub
-                    angleTextView.setText(" " + String.valueOf(angle) + "°");
-                    powerTextView.setText(" " + String.valueOf(power) + "%");
-                    switch (direction) {
-                        case JoystickView.FRONT:
-                            directionTextView.setText(R.string.front_lab);
-                            break;
-                        case JoystickView.FRONT_RIGHT:
-                            directionTextView.setText(R.string.front_right_lab);
-                            break;
-                        case JoystickView.RIGHT:
-                            directionTextView.setText(R.string.right_lab);
-                            break;
-                        case JoystickView.RIGHT_BOTTOM:
-                            directionTextView.setText(R.string.right_bottom_lab);
-                            break;
-                        case JoystickView.BOTTOM:
-                            directionTextView.setText(R.string.bottom_lab);
-                            break;
-                        case JoystickView.BOTTOM_LEFT:
-                            directionTextView.setText(R.string.bottom_left_lab);
-                            break;
-                        case JoystickView.LEFT:
-                            directionTextView.setText(R.string.left_lab);
-                            break;
-                        case JoystickView.LEFT_FRONT:
-                            directionTextView.setText(R.string.left_front_lab);
-                            break;
-                        default:
-                            directionTextView.setText(R.string.center_lab);
+            @Override
+            public void onValueChanged(int angle, int power, int direction) {
+                // TODO Auto-generated method stub
+                angleTextView.setText(" " + String.valueOf(angle) + "°");
+                powerTextView.setText(" " + String.valueOf(power) + "%");
+                switch (direction) {
+                    case JoystickView.FRONT:
+                        directionTextView.setText(R.string.front_lab);
+                        break;
+                    case JoystickView.FRONT_RIGHT:
+                        directionTextView.setText(R.string.front_right_lab);
+                        break;
+                    case JoystickView.RIGHT:
+                        directionTextView.setText(R.string.right_lab);
+                        break;
+                    case JoystickView.RIGHT_BOTTOM:
+                        directionTextView.setText(R.string.right_bottom_lab);
+                        break;
+                    case JoystickView.BOTTOM:
+                        directionTextView.setText(R.string.bottom_lab);
+                        break;
+                    case JoystickView.BOTTOM_LEFT:
+                        directionTextView.setText(R.string.bottom_left_lab);
+                        break;
+                    case JoystickView.LEFT:
+                        directionTextView.setText(R.string.left_lab);
+                        break;
+                    case JoystickView.LEFT_FRONT:
+                        directionTextView.setText(R.string.left_front_lab);
+                        break;
+                    default:
+                        directionTextView.setText(R.string.center_lab);
+                }
+            }
+        }, JoystickView.DEFAULT_LOOP_INTERVAL);
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        sensorManager.registerListener(listener, sensorAccel,
+                SensorManager.SENSOR_DELAY_NORMAL);
+        sensorManager.registerListener(listener, sensorLinAccel,
+                SensorManager.SENSOR_DELAY_NORMAL);
+        sensorManager.registerListener(listener, sensorGravity,
+                SensorManager.SENSOR_DELAY_NORMAL);
+
+        timer = new Timer();
+        TimerTask task = new TimerTask() {
+            @Override
+            public void run() {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        showInfo();
                     }
-                }
-            }, JoystickView.DEFAULT_LOOP_INTERVAL);
-        }
+                });
+            }
+        };
+        timer.schedule(task, 0, 400);
+    }
 
+    @Override
+    protected void onPause() {
+        super.onPause();
+        sensorManager.unregisterListener(listener);
+        timer.cancel();
+    }
+
+    String format(float values[]) {
+        return String.format("%1$.1f\t\t%2$.1f\t\t%3$.1f", values[0], values[1],
+                values[2]);
+    }
+
+    void showInfo() {
+        sb.setLength(0);
+        sb.append("Accelerometer: " + format(valuesAccel))
+                .append("\n\nAccel motion: " + format(valuesAccelMotion))
+                .append("\nAccel gravity : " + format(valuesAccelGravity))
+                .append("\n\nLin accel : " + format(valuesLinAccel))
+                .append("\nGravity : " + format(valuesGravity));
+
+        tvText.setText(sb);
+    }
+
+    float[] valuesAccel = new float[3];
+    float[] valuesAccelMotion = new float[3];
+    float[] valuesAccelGravity = new float[3];
+    float[] valuesLinAccel = new float[3];
+    float[] valuesGravity = new float[3];
+
+    SensorEventListener listener = new SensorEventListener() {
 
         @Override
-        protected void onResume() {
-            super.onResume();
-            sensorManager.registerListener(listener, sensorAccel,
-                    SensorManager.SENSOR_DELAY_NORMAL);
-            sensorManager.registerListener(listener, sensorLinAccel,
-                    SensorManager.SENSOR_DELAY_NORMAL);
-            sensorManager.registerListener(listener, sensorGravity,
-                    SensorManager.SENSOR_DELAY_NORMAL);
-
-            timer = new Timer();
-            TimerTask task = new TimerTask() {
-                @Override
-                public void run() {
-                    runOnUiThread(new Runnable() {
-                        @Override
-                        public void run() {
-                            showInfo();
-                        }
-                    });
-                }
-            };
-            timer.schedule(task, 0, 400);
+        public void onAccuracyChanged(Sensor sensor, int accuracy) {
         }
 
         @Override
-        protected void onPause() {
-            super.onPause();
-            sensorManager.unregisterListener(listener);
-            timer.cancel();
-        }
-
-        String format(float values[]) {
-            return String.format("%1$.1f\t\t%2$.1f\t\t%3$.1f", values[0], values[1],
-                    values[2]);
-        }
-
-        void showInfo() {
-                sb.setLength(0);
-                sb.append("Accelerometer: " + format(valuesAccel))
-                        .append("\n\nAccel motion: " + format(valuesAccelMotion))
-                        .append("\nAccel gravity : " + format(valuesAccelGravity))
-                        .append("\n\nLin accel : " + format(valuesLinAccel))
-                        .append("\nGravity : " + format(valuesGravity));
-
-                tvText.setText(sb);
+        public void onSensorChanged(SensorEvent event) {
+            switch (event.sensor.getType()) {
+                case Sensor.TYPE_ACCELEROMETER:
+                    for (int i = 0; i < 3; i++) {
+                        valuesAccel[i] = event.values[i];
+                        valuesAccelGravity[i] = (float) (0.1 * event.values[i] + 0.9 * valuesAccelGravity[i]);
+                        valuesAccelMotion[i] = event.values[i]
+                                - valuesAccelGravity[i];
+                    }
+                    break;
+                case Sensor.TYPE_LINEAR_ACCELERATION:
+                    for (int i = 0; i < 3; i++) {
+                        valuesLinAccel[i] = event.values[i];
+                    }
+                    break;
+                case Sensor.TYPE_GRAVITY:
+                    for (int i = 0; i < 3; i++) {
+                        valuesGravity[i] = event.values[i];
+                    }
+                    break;
             }
 
-            float[] valuesAccel = new float[3];
-            float[] valuesAccelMotion = new float[3];
-            float[] valuesAccelGravity = new float[3];
-            float[] valuesLinAccel = new float[3];
-            float[] valuesGravity = new float[3];
+        }
 
-            SensorEventListener listener = new SensorEventListener() {
-
-                @Override
-                public void onAccuracyChanged(Sensor sensor, int accuracy) {
-                }
-
-                @Override
-                public void onSensorChanged(SensorEvent event) {
-                    switch (event.sensor.getType()) {
-                        case Sensor.TYPE_ACCELEROMETER:
-                            for (int i = 0; i < 3; i++) {
-                                valuesAccel[i] = event.values[i];
-                                valuesAccelGravity[i] = (float) (0.1 * event.values[i] + 0.9 * valuesAccelGravity[i]);
-                                valuesAccelMotion[i] = event.values[i]
-                                        - valuesAccelGravity[i];
-                            }
-                            break;
-                        case Sensor.TYPE_LINEAR_ACCELERATION:
-                            for (int i = 0; i < 3; i++) {
-                                valuesLinAccel[i] = event.values[i];
-                            }
-                            break;
-                        case Sensor.TYPE_GRAVITY:
-                            for (int i = 0; i < 3; i++) {
-                                valuesGravity[i] = event.values[i];
-                            }
-                            break;
-                    }
-
-                }
-
-        };
-
-<<<<<<< Updated upstream
-    }
-=======
-    }
->>>>>>> Stashed changes
+    };
+}
